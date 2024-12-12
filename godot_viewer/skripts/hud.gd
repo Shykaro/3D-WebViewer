ba498746e7ec7d84dd627a42789e9911d4c94889
Shykaro <9372410+Shykaro@users.noreply.github.com>
2024-12-12 17:30:40 +0100

    adjusted backgrounds

--- godot_viewer/skripts/hud.gd
+++ godot_viewer/skripts/hud.gd
@@ -5,19 +5,20 @@ extends Control
 func _ready():
 	#if not check_button.is_connected("toggled", Callable(self, "_on_check_button_toggled")):
 		#check_button.toggled.connect(_on_check_button_toggled)
-	##change_camera_background_color(Color(1, 1, 1))  #Setze StandardEnvironment Color
+	change_camera_background_color(Color(1, 1, 1))  #Setze StandardEnvironment Color
 	pass
 
 func _on_check_button_toggled(button_pressed: bool):
-	if button_pressed:
-		change_camera_background_color(Color.html("#b9b9b9"))  # Hellgra, wenn aktiviert
-	else:
-		change_camera_background_color(Color.html("#313131"))  # Dunkelgrau, wenn deaktiviert
+	#if button_pressed:
+		#change_camera_background_color(Color.html("#b9b9b9"))  # Hellgra, wenn aktiviert
+	#else:
+		#change_camera_background_color(Color.html("#313131"))  # Dunkelgrau, wenn deaktiviert
+	return
 
 func change_camera_background_color(color: Color):
 	# Camera nach environment Source checken
 	if camera.environment == null:
 		camera.environment = Environment.new()
-
 	camera.environment.background_mode = Environment.BG_COLOR
 	camera.environment.background_color = color  # Setze die Hintergrundfarbe
+	return
